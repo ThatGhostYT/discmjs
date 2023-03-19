f3c12c541eb2ad95c5c989126e23ae6c2f77bed2
ThatGhostYT <82480584+ThatGhostYT@users.noreply.github.com>
2023-03-19 16:52:52 -0400

    add basic sveltekit site

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { TextGradient } from "@discmjs/ui";
+</script>
+
+<h1 id="title">
+    discm<TextGradient color1="pink" color2="mediumpurple">.js</TextGradient>
+</h1>
+
+<style>
+    #title{
+        font-size: 3em;
+        text-align: center;
+    }
+</style>
---
+++ packages/ui/components/TextGradient.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let color1: string;
+    export let color2: string;
+</script>
+
+<span id="gradient" style="--color1: {color1}; --color2: {color2}">
+    <slot>Content not provided.</slot>
+</span>
+
+<style>
+    #gradient{
+        background: linear-gradient(
+            var(--color1),
+            var(--color2)
+        );
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+</style>
